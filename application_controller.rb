c7147a7ea43633e2065df87dd507a07626b5c1f9
dfenjves <fenjves.daniel@gmail.com>
2015-06-04 13:16:44 +0000

    done

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,18 @@
 require 'bundler'
 Bundler.require
 
+require_relative 'models/pig_latinize'
+
 class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
   end
+  
+  post '/' do
+    @params = params
+    erb :results
+  end
+  
 
 end
